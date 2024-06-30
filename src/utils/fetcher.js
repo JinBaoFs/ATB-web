d273c73d4a2ca4193d3399fb8045066c3d2bae6f
JinBaoFs <13631531284@163.com>
2024-06-30 11:01:14 +0800

    修改

--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -19,7 +19,7 @@ export function fetcher(options) {
   }
   const config = {
     // 测试
-    baseURL: options?.baseURL || "http://192.168.1.179:8080",
+    baseURL: options?.baseURL || "http://18.143.16.138:8080",
     // baseURL: options?.baseURL || "/api",
     method: options.method || 'post',
     url: options.url,
